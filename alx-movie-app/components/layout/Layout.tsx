c67f31be515af61952b58685baac53c5700997a9
Shannia <shanniageofrey@gmail.com>
2025-08-10 22:13:12 +0300

    Set up Header

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,5 +1,16 @@
-export default function Layout(){
-    return(
-        <div></div>
-    )
+
+import { ComponentProps } from "@/interfaces";
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+
+export default function Layout({ children }: ComponentProps) {
+  return (
+    <div>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </div>
+  );
 }
